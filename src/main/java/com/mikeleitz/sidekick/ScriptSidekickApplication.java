bc6e444a63be7e52e8153ecb773c07a4a14eb368
Leitz <leitz@mikeleitz.com>
2019-08-19 13:38:13 -0500

    Add git flow structure.  Basic Spring boot and .gitignore.

---
+++ src/main/java/com/mikeleitz/sidekick/ScriptSidekickApplication.java
@@ -0,0 +1,16 @@
+package com.mikeleitz.sidekick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leitz@mikeleitz.com
+ */
+@SpringBootApplication
+public class ScriptSidekickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptSidekickApplication.class, args);
+	}
+
+}
