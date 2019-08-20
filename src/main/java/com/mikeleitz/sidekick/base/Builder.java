b0032165098eaa8d54f52f525b182bdc93a371ea
Leitz <leitz@mikeleitz.com>
2019-08-20 08:40:38 -0500

    Add copyright and license.

--- src/main/java/com/mikeleitz/sidekick/base/Builder.java
+++ src/main/java/com/mikeleitz/sidekick/base/Builder.java
@@ -13,19 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.mikeleitz.sidekick;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package com.mikeleitz.sidekick.base;
 
 /**
  * @author leitz@mikeleitz.com
  */
-@SpringBootApplication
-public class ScriptSidekickApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ScriptSidekickApplication.class, args);
-	}
-
+public interface Builder {
 }
