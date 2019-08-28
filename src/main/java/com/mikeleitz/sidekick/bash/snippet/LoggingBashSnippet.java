9d20ce306f310e75438496e400f48f8304282646
Leitz <leitz@mikeleitz.com>
2019-08-28 09:39:39 -0500

    Move classes around to new packages.

--- src/main/java/com/mikeleitz/sidekick/bash/snippet/LoggingBashSnippet.java
+++ src/main/java/com/mikeleitz/sidekick/bash/snippet/LoggingBashSnippet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.mikeleitz.sidekick.bash;
+package com.mikeleitz.sidekick.bash.snippet;
 
 import com.mikeleitz.sidekick.base.Snippet;
 import com.mikeleitz.sidekick.base.SnippetContext;
