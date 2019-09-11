341ef6ac0e38005ec8a8ca762c9765bca0a0c837
leitz <leitz@mikeleitz.com>
2019-09-10 21:17:04 -0500

    Start creating input components and vue implementation.

--- ui/src/views/ConfigureScript.vue
+++ ui/src/views/ConfigureScript.vue
@@ -15,9 +15,60 @@
   -->
 
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <ScriptInput msg="Welcome to Your Vue.js App"/>
+
+  <div class="script-form">
+    <main role="main" class="container">
+      <form>
+        <fieldset class="scheduler-border">
+          <legend class="scheduler-border">Script Details</legend>
+
+          <div class="form-group">
+            <label for="scriptName">Name</label>
+
+            <input type="text" class="form-control" id="scriptName" placeholder="Script Name">
+          </div>
+
+          <div class="form-group">
+            <label for="scriptShell">Shell</label>
+
+            <div id="scriptShell">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="scriptShell" id="bashShell"
+                       value="bash" checked>
+                <label class="form-check-label" for="bashShell">
+                  Bash
+                </label>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset class="scheduler-border">
+          <legend class="scheduler-border">Script Inputs</legend>
+
+          <div class="form-group">
+            <label for="addCommonInputButtons">Common Inputs</label>
+
+            <div id="addCommonInputButtons" class="input-group mb-3">
+              <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <b-button :pressed.sync="verboseCommandAdded" variant="outline-primary" >Verbose</b-button>
+<!--                <p>Verbose State: <strong>{{ verboseCommandAdded }}</strong></p>-->
+                <b-button :pressed.sync="quietCommandAdded" variant="outline-primary">Quiet</b-button>
+<!--                <p>Quiet State: <strong>{{ quietCommandAdded }}</strong></p>-->
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <hr>
+          </div>
+
+          <ScriptInput msg="Welcome to Your Vue.js App"/>
+
+        </fieldset>
+      </form>
+    </main>
+
   </div>
 </template>
 
@@ -27,8 +78,37 @@
 
     export default {
         name: 'configure-script',
+        data() {
+            return {
+                verboseCommandAdded: false,
+                quietCommandAdded: false
+            }
+        },
         components: {
             ScriptInput
         }
     }
 </script>
+
+<style>
+  .script-form {
+    text-align: left;
+  }
+
+  fieldset.scheduler-border {
+    border: 1px groove #ddd !important;
+    padding: 0 1.4em 1.4em 1.4em !important;
+    margin: 0 0 1.5em 0 !important;
+    -webkit-box-shadow: 0px 0px 0px 0px #000;
+    box-shadow: 0px 0px 0px 0px #000;
+  }
+
+  legend.scheduler-border {
+    font-size: 1.2em !important;
+    font-weight: bold !important;
+    text-align: left !important;
+    width: auto;
+    padding: 0 10px;
+    border-bottom: none;
+  }
+</style>
