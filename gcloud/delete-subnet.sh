0dd6acbbe87d1ff2fe5f95b1bd2f41fdd5413250
leitz <leitz@mikeleitz.com>
2019-12-22 22:14:05 -0600

    Commit checkpoint.  Create cluster scripts. Still needs work.

---
+++ gcloud/delete-subnet.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# Load common variables for all deployment scripts.
+. ./setup-env.sh
+
+gcloud compute --project="${PROJECT_NAME}" networks subnets delete "${GKE_SUBNET_NAME}" --network="${VPC_NAME}" --region="${REGION}"
---
+++ gcloud/setup-env.sh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+
+PROJECT_NAME=side-script
+
+# dev
+ENVIRONMENT=dev
+
+# prod
+#ENVIRONMENT=prod
+
+# Physical resources
+REGION=us-central1
+ZONE=us-central1-a
+GKE_MACHINE_TYPE=g1-small
+
+# Networking Resources
+
+###
+# We have a /24 ip range for our whole dev VPC.  It's divided into two halves.  The first half is usable for
+# non-kubernetes addresses.  The upper half is reserved for our kubernetes cluster/nodes/pods/services.
+# Whole dev environment: 192.168.3.0/24
+###
+
+VPC_NAME=$PROJECT_NAME-vpc-$ENVIRONMENT
+NON_GKE_SUBNET_NAME=$PROJECT_NAME-net-sn-$ENVIRONMENT
+GKE_SUBNET_NAME=$PROJECT_NAME-gke-clust-net-sn-$ENVIRONMENT
+
+# Dev
+NON_GKE_SUBNET_IP_RANGE=10.10.12.0/24
+# GKE_CLUSTER_SUBNET_IP_RANGE must account for all nodes, pods, services, etc.  Every
+# instance with an ip address within the cluster.
+GKE_CLUSTER_NODE_SUBNET_IP_RANGE=10.10.10.0/24
+GKE_CLUSTER_POD_SUBNET_IP_RANGE=10.1.0.0/16
+GKE_CLUSTER_SERVICE_SUBNET_IP_RANGE=10.10.11.0/24
+
+# Prod
+#NON_GKE_SUBNET_IP_RANGE=192.168.4.0/25
+#GKE_SUBNET_IP_RANGE=192.168.4.128/25
+
+# Compute Resources
+
+# Kubernetes cluster
+GKE_CLUSTER_NAME=$PROJECT_NAME-gke-clust-$ENVIRONMENT
+GKE_CLUSTER_NODES=2
+
+###
+# With 8 pods per node we need 16 ip addresses for each node.  GKE allocates max pods
+# times 2 for the ip range.  So for max 8 pods per node is 8*2=16 ip addresses which
+# corresponds to /28 cidr block.
+###
+
+GKE_CLUSTER_MAX_PODS_PER_NODE=8
+
+
+
+# Storage Resources
