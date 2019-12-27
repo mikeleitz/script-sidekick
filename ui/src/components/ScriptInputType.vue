32554d817428f6874943639ca07f13a0d6b78a9c
leitz <leitz@mikeleitz.com>
2019-12-27 15:23:25 -0600

    Checkpoint.  Have basic skeleton for type selection.

---
+++ ui/src/components/ScriptInputType.vue
@@ -0,0 +1,61 @@
+<!--
+  -  Copyright (c) 2019, Michael Leitz
+  -  <p/>
+  -  Licensed under the Apache License, Version 2.0 (the "License");
+  -  you may not use this file except in compliance with the License.
+  -  You may obtain a copy of the License at
+  -  <p/>
+  -  http://www.apache.org/licenses/LICENSE-2.0
+  -  <p/>
+  -  Unless required by applicable law or agreed to in writing, software
+  -  distributed under the License is distributed on an "AS IS" BASIS,
+  -  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  -  See the License for the specific language governing permissions and
+  -  limitations under the License.
+  -->
+<template>
+  <div>
+    <b-form-group label="Data type"
+                  label-for="input-type-selection">
+      <b-button-group id="input-type-selection" class="mr-5">
+        <b-button size="sm" variant="outline-primary">Number</b-button>
+        <b-button size="sm" variant="outline-primary">String</b-button>
+        <b-button size="sm" variant="outline-primary">Boolean</b-button>
+        <b-button size="sm" variant="outline-primary">Other</b-button>
+      </b-button-group>
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+import { store } from '../store.js'
+// import { ValidationProvider } from 'vee-validate'
+
+export default {
+  name: 'ScriptInputType',
+  components: {
+    // ValidationProvider
+  },
+  props: {
+    // Script input id for this type component.
+    id: {
+      required: false,
+      type: Number,
+      default: -1
+    }
+  },
+  created () {
+    this.thisScriptInput = store.getScriptInputById(this.id)
+  },
+  data () {
+    return {
+      thisScriptInput: {}
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
