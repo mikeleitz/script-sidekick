8fa8a180800be3350b099441e370ee104e4a2c18
leitz <leitz@mikeleitz.com>
2020-09-18 20:04:33 -0400

    Fix for space automation.

--- .space.kts
+++ .space.kts
@@ -1,3 +1,9 @@
-job("Build and create Docker container") {
+job("Build") {
     gradlew("openjdk:11", "clean", "build", "jibDockerBuild", "docker")
+}
+
+job("create Docker container") {
+    docker {
+        gradlew("openjdk:11", "jibDockerBuild", "docker")
+    }
 }
