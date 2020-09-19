93a07828f10fb8094c7ce3d57e327d9e5a5b18d8
leitz <leitz@mikeleitz.com>
2020-09-18 20:21:02 -0400

    Can't quite get the space automation to work.

--- .space.kts
+++ .space.kts
@@ -1,15 +1,15 @@
-job("Build") {
-    gradlew("openjdk:11", "clean", "build")
-}
-
-job("create Docker container") {
-    docker {
-        build {
-            file = "ui/Dockerfile"
-        }
-
-        push("leadtechnologist.registry.jetbrains.space/mydocker/lickety-script-ui") {
-            tag = "0.0.1-SNAPSHOT"
-        }
-    }
-}
+//job("Build") {
+//    gradlew("openjdk:11", "clean", "build")
+//}
+//
+//job("create Docker container") {
+//    docker {
+//        build {
+//            file = "ui/Dockerfile"
+//        }
+//
+//        push("leadtechnologist.registry.jetbrains.space/mydocker/lickety-script-ui") {
+//            tag = "0.0.1-SNAPSHOT"
+//        }
+//    }
+//}
